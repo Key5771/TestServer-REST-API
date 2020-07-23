951770d3a0f3ce30e9802a93169c4588228ffb20
key5771 <ksj57715@hanmail.net>
2020-07-23 15:01:59 +0900

    RegistViewController 클래스 생성

---
+++ TestServer REST API Test/TestServer REST API Test/UI/Login/RegistViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistViewController.swift
+//  TestServer REST API Test
+//
+//  Created by 김기현 on 2020/07/23.
+//  Copyright © 2020 김기현. All rights reserved.
+//
+
+import UIKit
+
+class RegistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
