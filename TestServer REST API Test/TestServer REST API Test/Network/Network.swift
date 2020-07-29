613d691cbc3e253ee7cd8c20caa7c1be1b5b236e
key5771 <ksj57715@hanmail.net>
2020-07-29 16:33:32 +0900

    response 함수에서 파라미터가 nil인 경우와 nil이 아닌 경우 구분

--- TestServer REST API Test/TestServer REST API Test/Network/Network.swift
+++ TestServer REST API Test/TestServer REST API Test/Network/Network.swift
@@ -24,21 +24,34 @@ class Network {
     
     func response<Response: Decodable> (api: API,
                                         method: Alamofire.HTTPMethod,
-                                        parameters: Parameters? = nil,
+                                        parameters: Int? = nil,
                                         encoding: URLEncoding? = nil,
                                         completion handler: @escaping (Response) -> Void) {
         
-        AF.request(baseUrl + api.rawValue,
-                   method: method,
-                   parameters: parameters)
-            .responseDecodable(of: Response.self, queue: myQueue) { (response) in
-                
-                switch response.result {
+        if let params = parameters {
+            AF.request(baseUrl + api.rawValue + String(params), method: method).responseDecodable(of: Response.self, queue: myQueue) { (res) in
+                switch res.result {
                 case .success(let obj):
+                    print("parameter is not nil")
                     handler(obj)
                 case .failure(let err):
-                    print("Failure Error: \(err)")
+                    print("Error: \(err)")
                 }
+            }
+        } else {
+            AF.request(baseUrl + api.rawValue,
+                       method: method,
+                       parameters: parameters)
+                .responseDecodable(of: Response.self, queue: myQueue) { (response) in
+                    
+                    switch response.result {
+                    case .success(let obj):
+                        print("parameter is nil")
+                        handler(obj)
+                    case .failure(let err):
+                        print("Failure Error: \(err)")
+                    }
+            }
         }
     }
     
