e9fc532111d5a6a3bc2ef192997e0b61dc22cb06
key5771 <ksj57715@hanmail.net>
2020-07-29 16:34:34 +0900

    contentId를 통해 서버에 파라미터로 전달

--- TestServer REST API Test/TestServer REST API Test/UI/ContentViewController.swift
+++ TestServer REST API Test/TestServer REST API Test/UI/ContentViewController.swift
@@ -7,22 +7,37 @@
 //
 
 import UIKit
+import Alamofire
 
 class ContentViewController: UIViewController {
     @IBOutlet weak var userLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
     
+    var contentId = 0
     var data = TestData(id: nil, title: nil, content: nil, user: nil, time: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        userLabel.text = data.user! + " | " + data.time!
-        titleLabel.text = data.title
-        contentLabel.text = data.content
+        print("contentId: \(contentId)")
+//        userLabel.text = data.user! + " | " + data.time!
+//        titleLabel.text = data.title
+//        contentLabel.text = data.content
+        getContent(contentId)
     }
 
+    func getContent(_ id: Int) {
+        Network.shared.response(api: .getData, method: .get, parameters: contentId) { (response: TestData) in
+            print("responseData: \(response)")
+            self.data = response
+            
+            DispatchQueue.main.async {
+                self.userLabel.text = self.data.user! + " | " + self.data.time!
+                self.titleLabel.text = self.data.title
+                self.contentLabel.text = self.data.content
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
