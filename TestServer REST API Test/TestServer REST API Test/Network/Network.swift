75d14b89dd57e3b1de70d33567f86a55f8e5b905
key5771 <ksj57715@hanmail.net>
2020-07-29 16:43:11 +0900

    파라미터가 nil인 경우와 nil이 아닌 경우를 미리 판단하여 중복되는 코드 제거

--- TestServer REST API Test/TestServer REST API Test/Network/Network.swift
+++ TestServer REST API Test/TestServer REST API Test/Network/Network.swift
@@ -28,29 +28,18 @@ class Network {
                                         encoding: URLEncoding? = nil,
                                         completion handler: @escaping (Response) -> Void) {
         
+        var id = ""
+        
         if let params = parameters {
-            AF.request(baseUrl + api.rawValue + String(params), method: method).responseDecodable(of: Response.self, queue: myQueue) { (res) in
-                switch res.result {
-                case .success(let obj):
-                    print("parameter is not nil")
-                    handler(obj)
-                case .failure(let err):
-                    print("Error: \(err)")
-                }
-            }
-        } else {
-            AF.request(baseUrl + api.rawValue,
-                       method: method,
-                       parameters: parameters)
-                .responseDecodable(of: Response.self, queue: myQueue) { (response) in
-                    
-                    switch response.result {
-                    case .success(let obj):
-                        print("parameter is nil")
-                        handler(obj)
-                    case .failure(let err):
-                        print("Failure Error: \(err)")
-                    }
+            id = String(params)
+        }
+        
+        AF.request(baseUrl + api.rawValue + id, method: method).responseDecodable(of: Response.self, queue: myQueue) { (response) in
+            switch response.result {
+            case .success(let data):
+                handler(data)
+            case .failure(let err):
+                print("Error in GET: \(err)")
             }
         }
     }
